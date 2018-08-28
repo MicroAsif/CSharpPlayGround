a55b94b259696818aedb92f6394a7e7910359f28
Asif Rahman <asif0531@live.com>
2018-08-28 14:08:49 +0600

    hammingDistance added

--- CSharpPlayGround/Algorithm/Strings/HammingDistance.cs
+++ CSharpPlayGround/Algorithm/Strings/HammingDistance.cs
@@ -8,6 +8,19 @@ namespace CSharpPlayGround.Algorithm.Strings
 {
     class HammingDistance
     {
+        public int Test(string a, string b)
+        {
+            int distance = 0;
+            if (a.Length != b.Length)
+                Console.WriteLine("string length must be same");
 
+            for(int i = 0; i< a[i]; i++)
+            {
+                if (a[i] != b[i])
+                    distance++;
+            }
+
+            return distance;
+        }
     }
 }
