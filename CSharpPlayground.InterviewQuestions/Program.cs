715431ee89aee3898b26f70568dce02514aa094c
Asif Rahman <asif0531@live.com>
2018-09-14 19:23:47 +0600

    some interview question practiced

---
+++ CSharpPlayground.InterviewQuestions/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPlayground.InterviewQuestions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Method override and method hidding 
+            Base b = new Derived();
+            b.F1();
+            b.F2();
+            b.F3();
+
+
+            Z z = new Z();
+            z.Set(10, 20);
+            Console.WriteLine(z.x + " " + z.y);
+
+            //Can't access const with an instance
+            //Console.WriteLine(z.C1 + " " + z.C2);
+
+            Display(1,2,3);
+            int[] a = { 1, 2, 3, 4, 5 };
+            Display(a);
+
+
+            Console.Read();
+        }
+
+
+        public static void Display(params int[] b)
+        {
+            foreach (var item in b)
+            {
+                Console.WriteLine("Array is having : {0}", item);
+            }
+
+        }
+    }
+}
---
+++ CSharpPlayground.InterviewQuestions/SequenceFunction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPlayground.InterviewQuestions
+{
+    class Base
+    {
+        public Base()
+        {
+            Console.WriteLine("Base");
+        }
+        public void F1() => Console.WriteLine("Base F1");
+        public virtual void F2() => Console.WriteLine("Base F2");
+        public virtual void F3() => Console.WriteLine("Base F3");
+    }
+    class Derived : Base
+    {
+        public Derived()
+        {
+            Console.WriteLine("Derived");
+        }
+        public new void F1() => Console.WriteLine("Derived F1");
+        public override void F2() => Console.WriteLine("Derived F2");
+        public new void F3() => Console.WriteLine("Derived F3");
+    }
+}
